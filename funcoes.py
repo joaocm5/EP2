f5aa2e64a13206adc92766e700835b04c5e4d70a
Joao Castro Martins <jcastromartins@icloud.com>
2026-04-09 10:43:17 -0300

    Exercício 2 completo

--- funcoes.py
+++ funcoes.py
@@ -5,3 +5,16 @@ def rolar_dados(numero):
         valor = random.randint(1, 6)
         dados.append(valor)
     return dados
+
+#exercicio 2
+def guardar_dado(dados_rolados, estoque, dado_a_guardar):
+    rolados_tirando_guardado = []
+
+    for dado in dados_rolados:
+        if dado == dado_a_guardar:
+            estoque.append(dado)
+        else:
+            rolados_tirando_guardado.append(dado)
+
+    return [rolados_tirando_guardado, estoque]
+    
